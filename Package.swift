5fa293ebebbfe7f9d33fd07c1ae5d573eadd8257
o2alexanderfedin <af@O2.services>
2025-09-20 09:42:30 -0700

    chore: update minimum iOS target to 15.0 for actor/async support

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
     name: "NolockOCRClient",
     platforms: [
-        .iOS(.v11),
-        .macOS(.v10_13),
-        .tvOS(.v11),
-        .watchOS(.v4),
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
